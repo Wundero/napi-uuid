4435c15c6cb58661753bf27329bb6a8bd6795e14
Wundero <sublimecraftftw@gmail.com>
2024-06-24 05:19:15 -0700

    Try additional optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@ use uuid::Uuid;
 #[macro_use]
 extern crate napi_derive;
 
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[napi]
+#[inline(always)]
 pub fn v7() -> String {
   Uuid::now_v7().to_string()
 }
